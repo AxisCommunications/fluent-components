07c5a8719e462dc6962c6dcc9b11608fb4b274f3
jimmy <jimmy.svensson@axis.com>
2023-09-22 18:42:16 +0200

    chore: add vite base path

--- examples/vite.config.ts
+++ examples/vite.config.ts
@@ -5,26 +5,21 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/fluent-components/",
   resolve: {
     alias: {
-      "@axiscommunications/fluent-icons": path.resolve(
-        "../icons/src/index.ts"
-      ),
+      "@axiscommunications/fluent-icons": path.resolve("../icons/src/index.ts"),
       "@axiscommunications/fluent-stepper": path.resolve(
         "../components/stepper/src/index.ts"
       ),
       "@axiscommunications/fluent-password-input": path.resolve(
         "../components/password-input/src/index.ts"
       ),
-      "@axiscommunications/fluent-theme": path.resolve(
-        "../theme/src/index.ts"
-      ),
+      "@axiscommunications/fluent-theme": path.resolve("../theme/src/index.ts"),
       "@axiscommunications/fluent-topbar": path.resolve(
         "../components/topbar/src/index.ts"
       ),
-      "@axiscommunications/fluent-hooks": path.resolve(
-        "../hooks/src/index.ts"
-      ),
+      "@axiscommunications/fluent-hooks": path.resolve("../hooks/src/index.ts"),
       "@axiscommunications/fluent-styles": path.resolve(
         "../styles/src/index.ts"
       ),
