a5218595584644974f78f1b968acbc6d22ab21f7
jimmy <jimmy.svensson@axis.com>
2023-10-23 08:29:18 +0200

    chore: playwright ts config

--- examples/playwright.config.ts
+++ examples/playwright.config.ts
@@ -1,58 +1,13 @@
-import { defineConfig, devices } from "@playwright/test";
+import { PlaywrightTestConfig } from "@playwright/test";
+import { chromium, createConfig } from "./playwright.confg.base";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
-export default defineConfig({
-  testDir: "./test",
-  outputDir: "./test-results",
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
-  },
-
-  /* Configure projects for major browsers */
-  projects: [
+const config: PlaywrightTestConfig = {
+  ...createConfig([
     {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      name: "fluent-components:stest:chromium",
+      use: chromium(),
+      testMatch: ["**/**.stest.ts"],
     },
-
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
-
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: "pnpm run dev",
-  //   url: "http://127.0.0.1:5173/fluent-components/",
-  //   reuseExistingServer: !process.env.CI,
-  // },
-});
+  ]),
+};
+export default config;
