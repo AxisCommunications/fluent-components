4dc4103a2f0e88340b7d65937e4e495fa4f0a931
jimmy <jimmy.svensson@axis.com>
2023-10-27 11:21:28 +0200

    chore: add playwright system test

---
+++ examples/system-test/home-page.stest.ts
@@ -0,0 +1,10 @@
+import { expect, test } from "@playwright/test";
+import { goToPage } from "./utils";
+
+test("should be able to navigate to home page", async ({ page }) => {
+  await goToPage(page);
+
+  const homePage = page.locator("#welcome-page");
+
+  await expect(homePage).toBeVisible();
+});
