1c58e0db5155604df65fc30f157fd58cece24b27
jimmy <jimmy.svensson@axis.com>
2023-10-27 10:42:20 +0200

    chore: format

--- examples/system-test/home-page.stest.ts
+++ examples/system-test/home-page.stest.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import { goToPage } from "./utils";
 
 test("should be able to navigate to home page", async ({ page }) => {
