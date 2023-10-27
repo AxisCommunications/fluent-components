2cad341ea28df5ff13a41a1b4e0b4998750ea1ce
jimmy <jimmy.svensson@axis.com>
2023-10-27 10:20:56 +0200

    chore: testest2

--- examples/system-test/home-page.stest.ts
+++ examples/system-test/home-page.stest.ts
@@ -2,7 +2,9 @@ import { test, expect } from "@playwright/test";
 import { goToPage } from "./utils";
 
 test("should be able to navigate to home page", async ({ page }) => {
-  await goToPage(page);
+  // await goToPage(page);
+
+  page.goto("https://axiscommunications.github.io/fluent-components/");
 
   const homePage = page.locator("#welcome-page");
 
