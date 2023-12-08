e13de5cc6b4e7b1318cecefe30e5bd27e8eb4628
jimmy <jimmy.svensson@axis.com>
2023-12-08 11:10:54 +0100

    chore: linting

--- icons/build-react.ts
+++ icons/build-react.ts
@@ -99,7 +99,7 @@ function processFolder(srcPath, destPath, resizable) {
   /** @type string[] */
   const iconExports: string[] = [];
 
-  files.forEach(function (file, index) {
+  files.forEach(function(file, index) {
     var srcFile = path.join(srcPath, file);
     if (fs.lstatSync(srcFile).isDirectory()) {
       // for now, ignore subdirectories/localization, until we have a plan for handling it
@@ -128,9 +128,10 @@ function processFolder(srcPath, destPath, resizable) {
         [...iconContent.matchAll(new RegExp(`(?<= ${key}=)".+?"`, "g"))].map(
           (v) => v[0]
         );
-      const width = resizable ? '"1em"' : getAttr("width")[0];
+      const width = resizable ? "\"1em\"" : getAttr("width")[0];
       const paths = getAttr("d").join(",");
-      var jsCode = `export const ${destFilename} = (/*#__PURE__*/createFluentIcon('${destFilename}', ${width}, [${paths}]));`;
+      var jsCode =
+        `export const ${destFilename} = (/*#__PURE__*/createFluentIcon('${destFilename}', ${width}, [${paths}]));`;
       iconExports.push(jsCode);
     }
   });
