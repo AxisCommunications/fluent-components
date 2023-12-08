0e064c42ebbc2d93417267b0d85ac1a7b0e3551d
jimmy <jimmy.svensson@axis.com>
2023-12-08 13:26:10 +0100

    chore: restore updated type

--- icons/src/utils/FluentIconsProps.types.ts
+++ icons/src/utils/FluentIconsProps.types.ts
@@ -2,12 +2,10 @@ import React from "react";
 export type AxisIconProps<
   TBaseAttributes extends
     | React.SVGAttributes<SVGElement>
-    | React.HTMLAttributes<HTMLElement> = React.SVGAttributes<SVGElement>,
-  TRefType extends HTMLElement | SVGSVGElement = SVGSVGElement
-> = TBaseAttributes &
-  React.RefAttributes<TRefType> & {
-    primaryFill?: string;
-    className?: string;
-    filled?: boolean;
-    title?: string;
-  };
+    | React.HTMLAttributes<HTMLElement> = React.SVGAttributes<SVGElement>
+> = TBaseAttributes & {
+  primaryFill?: string;
+  className?: string;
+  filled?: boolean;
+  title?: string;
+};
