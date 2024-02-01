7ef6db1b977a6e70003573667a4be5a92c652b26
jimmy <jimmy.svensson@axis.com>
2024-02-01 08:02:50 +0100

    chore: redo codeblock example

--- examples/src/stories/tab-list-utilities/tab-list-styled..tsx
+++ examples/src/stories/tab-list-utilities/tab-list-styled..tsx
@@ -20,64 +20,18 @@ import {
 } from "@axiscommunications/fluent-styles";
 ...
 
-const HomeIcon = bundleIcon(HomeFilled, HomeRegular);
+//standard usage
+const { rootStyle: tabListStyle } = useTabListStyles({ vertical: true/false });
+const { rootStyle: tabStyle } = useTabStyles({ selected: true/false });
 
-export type TTabListComponent = {
-  withText?: boolean;
-} & TabListProps;
+<Tab className={tabListStyle} {...TabListProps}>
+    <Tab className={tabStyle} {...TabProps}>tab1</Tab>
+</Tab>
 
-export function StyledTabListComponent(
-  { withText = true, ...props }: TTabListComponent
-) {
-  const { selectedTab, setSelectedTab } = useTabListContext();
-  const { rootStyle } = useTabListStyles({ vertical: props.vertical });
-
-  return (
-    <TabList
-      selectedValue={selectedTab}
-      className={rootStyle}
-      defaultSelectedValue={selectedTab}
-      onTabSelect={(_, { value }) => {
-        setSelectedTab(value as unknown as string);
-      }}
-      {...props}
-    >
-      <StyledTabComponent
-        icon={<HomeIcon />}
-        value="tab1"
-        selected={selectedTab === "tab1"}
-      >
-        {withText && "First Tab"}
-      </StyledTabComponent>
-      <StyledTabComponent
-        icon={<HomeIcon />}
-        value="tab2"
-        selected={selectedTab === "tab2"}
-      >
-        {withText && "First Tab"}
-      </StyledTabComponent>
-      <StyledTabComponent
-        icon={<HomeIcon />}
-        value="tab3"
-        selected={selectedTab === "tab3"}
-      >
-        {withText && "First Tab"}
-      </StyledTabComponent>
-    </TabList>
-  );
-}
-
-export type TStyledTabComponent = {
-  selected?: boolean;
-} & TabProps;
-
-function StyledTabComponent(
-  { selected, children, ...props }: TStyledTabComponent
-) {
-  const { rootStyle } = useTabStyles({ selected });
-
-  return <Tab className={rootStyle} {...props}>{children}</Tab>;
-}
+//not happy with style? all styles can be grabbed from styles prop
+const { styles } = useTabListStyles();
+const newStyle = mergeClasses(styles.root, overrideStyles.root ...)
+...
 `;
 
 const HomeIcon = bundleIcon(HomeFilled, HomeRegular);
