138718045617bfb45a8070584cf380b84eb7fabf
Dilshod Aliev <dilshod.aliev@axis.com>
2024-02-08 16:56:50 +0100

    feat(passwordInput): enable ref forwarding

--- components/password-input/src/password-input.tsx
+++ components/password-input/src/password-input.tsx
@@ -1,36 +1,42 @@
 import React, { useCallback, useState } from "react";
 
-import { Input, mergeClasses } from "@fluentui/react-components";
+import {
+  ForwardRefComponent,
+  Input,
+  mergeClasses,
+} from "@fluentui/react-components";
 import { EyeOffRegular, EyeRegular } from "@fluentui/react-icons";
 
 import { PasswordInputProps } from "./password-input.types";
 import { usePasswordInputStyles } from "./password-input.styles";
 
-export function PasswordInput(props: PasswordInputProps) {
-  const { className, ...rest } = props;
+export const PasswordInput: ForwardRefComponent<PasswordInputProps> = React
+  .forwardRef((props: PasswordInputProps, ref) => {
+    const { className, ...rest } = props;
 
-  const styles = usePasswordInputStyles();
-  const inputStyles = mergeClasses(styles.edgeHack, className);
+    const styles = usePasswordInputStyles();
+    const inputStyles = mergeClasses(styles.edgeHack, className);
 
-  const [isVisible, setIsVisible] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
 
-  const togglePasswordVisibility = useCallback(() => {
-    setIsVisible((prev) => !prev);
-  }, []);
+    const togglePasswordVisibility = useCallback(() => {
+      setIsVisible((prev) => !prev);
+    }, []);
 
-  const iconProps = {
-    style: { cursor: "pointer" },
-    onClick: togglePasswordVisibility,
-  };
+    const iconProps = {
+      style: { cursor: "pointer" },
+      onClick: togglePasswordVisibility,
+    };
 
-  return (
-    <Input
-      type={isVisible ? "text" : "password"}
-      contentAfter={isVisible
-        ? <EyeOffRegular {...iconProps} />
-        : <EyeRegular {...iconProps} />}
-      className={inputStyles}
-      {...rest}
-    />
-  );
-}
+    return (
+      <Input
+        ref={ref}
+        type={isVisible ? "text" : "password"}
+        contentAfter={isVisible
+          ? <EyeOffRegular {...iconProps} />
+          : <EyeRegular {...iconProps} />}
+        className={inputStyles}
+        {...rest}
+      />
+    );
+  });
