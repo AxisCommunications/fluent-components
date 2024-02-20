1f3c2d274de65a356f726b47eab8c043b668ea16
jimmy <jimmy.svensson@axis.com>
2024-02-20 10:17:09 +0100

    chore: format

--- illustrations/illustrations/add.tsx
+++ illustrations/illustrations/add.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from "react";
 
 export function Example({
   size = 18, // or any default size of your choice
-  color = "black" // or any color of your choice
+  color = "black", // or any color of your choice
 }) {
   return (
     <svg
@@ -15,5 +15,5 @@ export function Example({
       <path fill="none" d="M0 0h24v24H0z" />
       <path d="M21 20a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V9.49a1 1 0 0 1 .386-.79l8-6.222a1 1 0 0 1 1.228 0l8 6.222a1 1 0 0 1 .386.79V20zm-2-1V9.978l-7-5.444-7 5.444V19h14z" />
     </svg>
-  )
+  );
 }
