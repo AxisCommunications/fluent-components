69a9260c67251f8c99a55e7fc7973849e1c8a652
Niklas Ramström <niklasra@axis.com>
2024-04-11 09:49:08 +0200

    fix(styles): update scrollbar static styles (#254)

--- styles/src/static/static.styles.ts
+++ styles/src/static/static.styles.ts
@@ -2,18 +2,20 @@ import { makeStaticStyles, tokens } from "@fluentui/react-components";
 
 const scrollbarColor = tokens.colorScrollbarOverlay;
 export const useScrollStaticStyles = makeStaticStyles({
-  "*": {
+  "@-moz-document url-prefix() {*": {
     // Firefox
+    // Scrollbar styling is amazingly complex. Read more here:
+    // https://stackoverflow.com/questions/6165472/custom-css-scrollbar-for-firefox
     scrollbarWidth: "thin",
     scrollbarColor: `${scrollbarColor} transparent`,
   },
   "*::-webkit-scrollbar": {
-    width: tokens.spacingHorizontalS,
-    height: tokens.spacingVerticalS,
+    width: tokens.spacingHorizontalSNudge,
+    height: tokens.spacingHorizontalSNudge,
   },
   "*::-webkit-scrollbar-thumb": {
     background: scrollbarColor,
-    borderRadius: tokens.borderRadiusLarge,
+    borderRadius: tokens.borderRadiusXLarge,
   },
   "*::-webkit-scrollbar-track": {
     background: "none",
