c5a50da7dc408b4a2d6cde795154764989d32eaf
Niklas Ramström <niklasra@axis.com>
2024-04-11 15:38:03 +0200

    fix(styles): handle firefox style without warning in chrome (#259)

--- styles/src/static/static.styles.ts
+++ styles/src/static/static.styles.ts
@@ -1,13 +1,15 @@
 import { makeStaticStyles, tokens } from "@fluentui/react-components";
 
 const scrollbarColor = tokens.colorScrollbarOverlay;
+
+const isFirefox = window.navigator.userAgent.includes("Fire");
 export const useScrollStaticStyles = makeStaticStyles({
-  "@-moz-document url-prefix() {*": {
+  "*": {
     // Firefox
     // Scrollbar styling is amazingly complex. Read more here:
     // https://stackoverflow.com/questions/6165472/custom-css-scrollbar-for-firefox
-    scrollbarWidth: "thin",
-    scrollbarColor: `${scrollbarColor} transparent`,
+    scrollbarWidth: isFirefox ? "thin" : "unset",
+    scrollbarColor: isFirefox ? `${scrollbarColor} transparent` : "auto",
   },
   "*::-webkit-scrollbar": {
     width: tokens.spacingHorizontalSNudge,
