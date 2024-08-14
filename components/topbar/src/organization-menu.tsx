5b3f7d0a2d5495018eca2c5b3d5bb9c82097e41b
Niklas Ramström <niklasra@axis.com>
2024-08-14 16:35:21 +0200

    feat(topbar): add ability to search organizations (#294)

--- components/topbar/src/organization-menu.tsx
+++ components/topbar/src/organization-menu.tsx
@@ -1,4 +1,7 @@
 import {
+  Button,
+  Input,
+  InputOnChangeData,
   Menu,
   MenuButton,
   MenuDivider,
@@ -12,8 +15,10 @@ import {
   BuildingMultipleFilled,
   BuildingMultipleRegular,
   bundleIcon,
+  Dismiss16Regular,
+  Search16Regular,
 } from "@fluentui/react-icons";
-import React from "react";
+import React, { useCallback, useRef, useState } from "react";
 import { OrganizationMenuProps } from "./organization-menu.types";
 import { useStyles } from "./organization.styles";
 
@@ -22,10 +27,17 @@ const OrganizationIcon = bundleIcon(
   BuildingMultipleRegular
 );
 
-export const OrganizationMenu = (
-  { customContent, onChange, options, value }: OrganizationMenuProps
-) => {
+export const OrganizationMenu = ({
+  customContent,
+  onChange,
+  options,
+  value,
+  filter,
+}: OrganizationMenuProps) => {
   const styles = useStyles();
+
+  const [filterText, setFilterText] = useState<string>("");
+  const filterRef = useRef<HTMLInputElement>(null);
 
   const currentOrganization = options?.find(({ id }) => id === value);
   const checkedValues = { org: [value] };
@@ -33,8 +45,31 @@ export const OrganizationMenu = (
     && currentOrganization;
   const onlyCustomContent = !options?.length && !!customContent;
 
+  const filteredOptions = options
+    ?.filter(
+      (opt) =>
+        filterText.length === 0
+        || opt.label.toLowerCase().indexOf(filterText) >= 0
+    )
+    .sort(
+      (a, b) =>
+        a.label.toLowerCase().indexOf(filterText)
+        - b.label.toLowerCase().indexOf(filterText)
+    );
+
+  const onFilterChange = useCallback(
+    (_: React.ChangeEvent<HTMLInputElement>, data: InputOnChangeData) => {
+      if (data.value.length) {
+        setFilterText(data.value.toLowerCase());
+      } else {
+        setFilterText("");
+      }
+    },
+    []
+  );
+
   return (
-    <Menu checkedValues={checkedValues}>
+    <Menu checkedValues={checkedValues} positioning={"below-end"}>
       <MenuTrigger>
         <MenuButton
           appearance="subtle"
@@ -45,6 +80,7 @@ export const OrganizationMenu = (
           data-testid="organization-menu-button"
           icon={<OrganizationIcon />}
           menuIcon={noDropDownContent ? null : undefined}
+          onClick={() => setFilterText("")}
         >
           <span className={styles.organizationlabel}>
             {currentOrganization?.label ?? value}
@@ -52,10 +88,41 @@ export const OrganizationMenu = (
         </MenuButton>
       </MenuTrigger>
       <MenuPopover>
+        {filter?.showFilter && (
+          <>
+            <Input
+              ref={filterRef}
+              contentBefore={<Search16Regular />}
+              placeholder={filter.placeholderText}
+              contentAfter={filterText.length
+                ? (
+                  <Button
+                    icon={<Dismiss16Regular />}
+                    appearance="transparent"
+                    onClick={() => {
+                      setFilterText("");
+                      filterRef.current?.focus();
+                    }}
+                  />
+                )
+                : undefined}
+              // To not get focus in search on open
+              tabIndex={-1}
+              // To keep focus on search when hovering menu items
+              onBlur={() => filterRef.current?.focus()}
+              className={styles.searchInput}
+              appearance="filled-lighter"
+              value={filterText}
+              onChange={onFilterChange}
+            />
+            <MenuDivider />
+          </>
+        )}
         <MenuList>
           {!onlyCustomContent && (
             <div className={styles.organizationSelection}>
-              {options?.map(({ id, label }) => {
+              {filteredOptions?.map(({ id, label }) => {
+                const match = label.toLowerCase().indexOf(filterText);
                 return (
                   <MenuItemRadio
                     data-testid={`organization-menu-item-${id}`}
@@ -65,7 +132,15 @@ export const OrganizationMenu = (
                     onClick={() => onChange(id)}
                     value={id}
                   >
-                    {label}
+                    {
+                      <span>
+                        {label.substring(0, match)}
+                        <span className={styles.bold}>
+                          {label.substring(match, match + filterText.length)}
+                        </span>
+                        {label.substring(match + filterText.length)}
+                      </span>
+                    }
                   </MenuItemRadio>
                 );
               })}
