a7c3a30ef72711bf210fcf29ce09ba4923634175
markuslindmark <markus.lindmark@axis.com>
2025-08-22 13:54:11 +0200

    style: lint fix

--- tools/generated/changelog/git.js
+++ tools/generated/changelog/git.js
@@ -1,24 +1,29 @@
 import { execSync } from "node:child_process";
 function sanitizeGitHubUrl(url) {
-    if (new URL(url).hostname !== "github.com") {
-        throw new Error("not implemented: only GitHub repositories are supported");
-    }
-    return url.replace(/\.git$/, "");
+  if (new URL(url).hostname !== "github.com") {
+    throw new Error("not implemented: only GitHub repositories are supported");
+  }
+  return url.replace(/\.git$/, "");
 }
-export const compareUrl = (url, range) => `${sanitizeGitHubUrl(url)}/compare/${range}`;
-export const commitUrl = (url, commit) => `${sanitizeGitHubUrl(url)}/commit/${commit}`;
+export const compareUrl = (url, range) =>
+  `${sanitizeGitHubUrl(url)}/compare/${range}`;
+export const commitUrl = (url, commit) =>
+  `${sanitizeGitHubUrl(url)}/commit/${commit}`;
 export function gitLogFromRange(range) {
-    try {
-        const logOut = execSync(`git log --no-merges --date-order --format="%H%x09%s" ${range}`);
-        const lines = logOut.toString().trim().split("\n");
-        const shaMessagePairs = lines.map((line) => line.split("\t"));
-        return shaMessagePairs;
-    }
-    catch {
-        console.warn(`git log failed on range ${range}, one of those tags probably does not exist`);
-        return [];
-    }
+  try {
+    const logOut = execSync(
+      `git log --no-merges --date-order --format="%H%x09%s" ${range}`
+    );
+    const lines = logOut.toString().trim().split("\n");
+    const shaMessagePairs = lines.map((line) => line.split("\t"));
+    return shaMessagePairs;
+  } catch {
+    console.warn(
+      `git log failed on range ${range}, one of those tags probably does not exist`
+    );
+    return [];
+  }
 }
 export function shortSha(sha) {
-    return execSync(`git log -1 --format=%h ${sha}`).toString().trim();
+  return execSync(`git log -1 --format=%h ${sha}`).toString().trim();
 }
--- tools/generated/commitlint/commitlint.js
+++ tools/generated/commitlint/commitlint.js
@@ -1,39 +1,38 @@
 import { execSync } from "node:child_process";
-const re = /^(?<type>build|chore|ci|docs|feat|fix|perf|refactor|release|revert|style|test)(?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?|^(?<merge>Merge \w+)$/gm;
+const re =
+  /^(?<type>build|chore|ci|docs|feat|fix|perf|refactor|release|revert|style|test)(?<scope>\(\w+\)?((?=:\s)|(?=!:\s)))?(?<breaking>!)?(?<subject>:\s.*)?|^(?<merge>Merge \w+)$/gm;
 function isConventionalCommitMessage(msg) {
-    const match = msg.match(re);
-    return match !== null && match[0] === msg;
+  const match = msg.match(re);
+  return match !== null && match[0] === msg;
 }
 function log(range) {
-    return execSync(`git rev-list ${range}`).toString().trim().split("\n");
+  return execSync(`git rev-list ${range}`).toString().trim().split("\n");
 }
 function message(sha) {
-    return execSync(`git log -1 --format=%s ${sha}`).toString().trim();
+  return execSync(`git log -1 --format=%s ${sha}`).toString().trim();
 }
 export function rangeLint(range = "HEAD~..HEAD") {
-    let rc = 0;
-    const commits = log(range);
-    console.log("verifying conventional commit message format:");
-    for (const commit of commits) {
-        const msg = message(commit);
-        if (!isConventionalCommitMessage(msg)) {
-            console.log("[FAIL]", msg);
-            rc = 1;
-        }
-        else {
-            console.log("[OK]", msg);
-        }
+  let rc = 0;
+  const commits = log(range);
+  console.log("verifying conventional commit message format:");
+  for (const commit of commits) {
+    const msg = message(commit);
+    if (!isConventionalCommitMessage(msg)) {
+      console.log("[FAIL]", msg);
+      rc = 1;
+    } else {
+      console.log("[OK]", msg);
     }
-    process.exit(rc);
+  }
+  process.exit(rc);
 }
 export function messageLint(msg) {
-    let rc = 0;
-    if (!isConventionalCommitMessage(msg)) {
-        console.log("[FAIL]", msg);
-        rc = 1;
-    }
-    else {
-        console.log("[OK]", msg);
-    }
-    process.exit(rc);
+  let rc = 0;
+  if (!isConventionalCommitMessage(msg)) {
+    console.log("[FAIL]", msg);
+    rc = 1;
+  } else {
+    console.log("[OK]", msg);
+  }
+  process.exit(rc);
 }
