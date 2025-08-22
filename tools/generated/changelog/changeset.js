d8d5db02109bd4cdb8bba0a5bfb3e8d1f39261b4
markuslindmark <markus.lindmark@axis.com>
2025-08-22 10:45:56 +0200

    style: new icon
    Added windshield washer icon

--- tools/generated/changelog/changeset.js
+++ tools/generated/changelog/changeset.js
@@ -1,84 +1,81 @@
 import { commitUrl, compareUrl, gitLogFromRange, shortSha } from "./git.js";
 const GroupTitles = {
-  build: "👷 Build",
-  chore: "🚧 Maintenance",
-  ci: "🚦 Continous integration",
-  docs: "📝 Documentation",
-  feat: "✨ Features",
-  fix: "🐛 Bug fixes",
-  perf: "🏎️ Performance",
-  refactor: "♻️ Refactoring",
-  revert: "⏪️ Reverts",
-  style: "💄 Styling",
-  test: "🧪 Test",
+    build: "👷 Build",
+    chore: "🚧 Maintenance",
+    ci: "🚦 Continous integration",
+    docs: "📝 Documentation",
+    feat: "✨ Features",
+    fix: "🐛 Bug fixes",
+    perf: "🏎️ Performance",
+    refactor: "♻️ Refactoring",
+    revert: "⏪️ Reverts",
+    style: "💄 Styling",
+    test: "🧪 Test",
 };
 const GroupKeys = new Set(Object.keys(GroupTitles));
 export function changeset({ date, name, range, scope, url }) {
-  return [
-    changesetHeader({ date, name, range, url }),
-    changesetBody({ range, scope, url }),
-  ].join("");
+    return [
+        changesetHeader({ date, name, range, url }),
+        changesetBody({ range, scope, url }),
+    ].join("");
 }
 function changesetHeader({ date, name, range, url }) {
-  if (url !== undefined) {
-    return `## [${name}](${compareUrl(url, range)}) (${date})\n`;
-  }
-  return `## ${name} (${date})\n`;
+    if (url !== undefined) {
+        return `## [${name}](${compareUrl(url, range)}) (${date})\n`;
+    }
+    return `## ${name} (${date})\n`;
 }
 function changesetBody({ range, scope, url }) {
-  const outputChunks = [];
-  const groups = {};
-  for (const [sha, msg] of gitLogFromRange(range)) {
-    const cc = parseConventionalCommitMessage(msg);
-    if (scope !== undefined && cc.scope !== scope) {
-      continue;
+    const outputChunks = [];
+    const groups = {};
+    for (const [sha, msg] of gitLogFromRange(range)) {
+        const cc = parseConventionalCommitMessage(msg);
+        if (scope !== undefined && cc.scope !== scope) {
+            continue;
+        }
+        if (groups[cc.group] === undefined) {
+            groups[cc.group] = [];
+        }
+        groups[cc.group].push([sha, cc]);
     }
-    if (groups[cc.group] === undefined) {
-      groups[cc.group] = [];
+    for (const group of GroupKeys) {
+        if (groups[group] === undefined) {
+            continue;
+        }
+        outputChunks.push(`\n### ${GroupTitles[group]}\n\n`);
+        for (const [sha, cc] of groups[group]) {
+            const scopePrefix = scope === undefined && cc.scope !== undefined
+                ? ` **${cc.scope}**:`
+                : "";
+            const breakingPrefix = cc.breaking ? ` **BREAKING**` : "";
+            const link = url !== undefined
+                ? `([${shortSha(sha)}](${commitUrl(url, sha)}))`
+                : `(${shortSha(sha)})`;
+            outputChunks.push(`  -${scopePrefix}${breakingPrefix} ${cc.title} ${link}\n`);
+        }
     }
-    groups[cc.group].push([sha, cc]);
-  }
-  for (const group of GroupKeys) {
-    if (groups[group] === undefined) {
-      continue;
-    }
-    outputChunks.push(`\n### ${GroupTitles[group]}\n\n`);
-    for (const [sha, cc] of groups[group]) {
-      const scopePrefix =
-        scope === undefined && cc.scope !== undefined
-          ? ` **${cc.scope}**:`
-          : "";
-      const breakingPrefix = cc.breaking ? ` **BREAKING**` : "";
-      const link =
-        url !== undefined
-          ? `([${shortSha(sha)}](${commitUrl(url, sha)}))`
-          : `(${shortSha(sha)})`;
-      outputChunks.push(
-        `  -${scopePrefix}${breakingPrefix} ${cc.title} ${link}\n`
-      );
-    }
-  }
-  return outputChunks.join("");
+    return outputChunks.join("");
 }
 function parseConventionalCommitMessage(msg) {
-  try {
-    const match = msg.match(/^([^:!(]+)(?:\(([^)]+)\))?(!)?: (.*)$/);
-    if (match === null) {
-      throw new Error("no matches found");
+    try {
+        const match = msg.match(/^([^:!(]+)(?:\(([^)]+)\))?(!)?: (.*)$/);
+        if (match === null) {
+            throw new Error("no matches found");
+        }
+        const [, group, scope, breaking, title] = match;
+        return {
+            group,
+            scope,
+            breaking: breaking !== undefined,
+            title,
+        };
     }
-    const [, group, scope, breaking, title] = match;
+    catch {
+        process.stderr.write(`invalid conventional commit message: ${msg}\n`);
+    }
     return {
-      group,
-      scope,
-      breaking: breaking !== undefined,
-      title,
+        group: "chore",
+        breaking: false,
+        title: msg,
     };
-  } catch {
-    process.stderr.write(`invalid conventional commit message: ${msg}\n`);
-  }
-  return {
-    group: "chore",
-    breaking: false,
-    title: msg,
-  };
 }
--- tools/generated/release/release.js
+++ tools/generated/release/release.js
@@ -3,46 +3,38 @@ import { readFileSync, writeFileSync } from "node:fs";
 import semver from "semver";
 import { changeset } from "../changelog/changeset.js";
 function commit(version) {
-  execSync(`git add -u`);
-  execSync(`git commit -m "release: v${version}"`);
+    execSync(`git add -u`);
+    execSync(`git commit -m "release: v${version}"`);
 }
 function sha(commitish) {
-  return execSync(`git rev-parse ${commitish}`).toString().trim();
+    return execSync(`git rev-parse ${commitish}`).toString().trim();
 }
 function bumpPackages(version) {
-  execSync(`pnpm version --no-git-tag-version ${version}`);
-  execSync(`pnpm -r exec pnpm version --no-git-tag-version ${version}`);
-  execSync("pnpm format");
+    execSync(`pnpm version --no-git-tag-version ${version}`);
+    execSync(`pnpm -r exec pnpm version --no-git-tag-version ${version}`);
+    execSync("pnpm format");
 }
 export function release(increment) {
-  const releaseType = semver.RELEASE_TYPES.find((r) => r === increment);
-  if (releaseType === undefined) {
-    throw new Error(
-      `unknown increment: ${increment}, expected one of ${semver.RELEASE_TYPES}`
-    );
-  }
-  const { version, repository } = JSON.parse(
-    readFileSync("package.json").toString()
-  );
-  const isPre = releaseType.startsWith("pre");
-  const nextVersion = semver.inc(
-    version,
-    releaseType,
-    isPre ? "alpha" : undefined
-  );
-  if (nextVersion === null) {
-    throw new Error(`could not increment ${version} with ${releaseType}`);
-  }
-  console.log(`${version} => ${nextVersion}`);
-  bumpPackages(nextVersion);
-  const from = sha(`v${version}`);
-  const to = sha("HEAD");
-  const changelog = changeset({
-    date: new Date().toISOString(),
-    name: nextVersion,
-    range: `${from}..${to}`,
-    url: repository.url,
-  });
-  writeFileSync("CHANGELOG.md", changelog);
-  commit(nextVersion);
+    const releaseType = semver.RELEASE_TYPES.find((r) => r === increment);
+    if (releaseType === undefined) {
+        throw new Error(`unknown increment: ${increment}, expected one of ${semver.RELEASE_TYPES}`);
+    }
+    const { version, repository } = JSON.parse(readFileSync("package.json").toString());
+    const isPre = releaseType.startsWith("pre");
+    const nextVersion = semver.inc(version, releaseType, isPre ? "alpha" : undefined);
+    if (nextVersion === null) {
+        throw new Error(`could not increment ${version} with ${releaseType}`);
+    }
+    console.log(`${version} => ${nextVersion}`);
+    bumpPackages(nextVersion);
+    const from = sha(`v${version}`);
+    const to = sha("HEAD");
+    const changelog = changeset({
+        date: new Date().toISOString(),
+        name: nextVersion,
+        range: `${from}..${to}`,
+        url: repository.url,
+    });
+    writeFileSync("CHANGELOG.md", changelog);
+    commit(nextVersion);
 }
