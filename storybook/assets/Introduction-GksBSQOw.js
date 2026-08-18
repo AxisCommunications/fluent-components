973eeddd0d9eeff069f5164bba75c5dc1ab031b7
tonyolsson <tonyolsson@users.noreply.github.com>
2026-08-18 13:49:51 +0000

    deploy: b581a7c11760cd84e6b8557315cb962430c339be

---
+++ storybook/assets/Introduction-GksBSQOw.js
@@ -0,0 +1,36 @@
+import{n as e}from"./rolldown-runtime-DkW27tQK.js";import{t}from"./jsx-runtime-DeHZSEgm.js";import{i as n,r}from"./react-Bl2r1tuC.js";import{p as i,u as a}from"./blocks-BVEdEjsg.js";function o(e){let t={code:`code`,h1:`h1`,h2:`h2`,li:`li`,ol:`ol`,p:`p`,ul:`ul`,...n(),...e.components};return(0,c.jsxs)(c.Fragment,{children:[`
+`,`
+`,(0,c.jsx)(a,{title:`Introduction`}),`
+`,(0,c.jsx)(t.h1,{id:`fluent-components`,children:`Fluent Components`}),`
+`,(0,c.jsx)(t.p,{children:`Welcome to the Axis Fluent component library for React and Fluent UI v9.
+This Storybook documents reusable building blocks and usage patterns used
+across applications.`}),`
+`,(0,c.jsx)(t.h2,{id:`packages-in-this-library`,children:`Packages In This Library`}),`
+`,(0,c.jsxs)(t.ul,{children:[`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-theme`}),` - Axis branded Fluent themes`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-icons`}),` - Axis icon set`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-hooks`}),` - shared React hooks`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-styles`}),` - shared style modules`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-stepper`}),` - stepper and wizard flows`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-slider`}),` - slider and range slider components`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-topbar`}),` - app, org, and profile menu topbar`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-empty-view`}),` - empty state views`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-password-input`}),` - password input with reveal`]}),`
+`,(0,c.jsxs)(t.li,{children:[(0,c.jsx)(t.code,{children:`@axiscommunications/fluent-illustrations`}),` - Axis branded illustrations`]}),`
+`]}),`
+`,(0,c.jsx)(t.h2,{id:`usage-patterns`,children:`Usage Patterns`}),`
+`,(0,c.jsx)(t.p,{children:`The usage patterns documented in this Storybook are developed by the
+Design System Group and serve as global UX patterns.`}),`
+`,(0,c.jsx)(t.p,{children:`Use them as the default approach when building new experiences to ensure:`}),`
+`,(0,c.jsxs)(t.ul,{children:[`
+`,(0,c.jsx)(t.li,{children:`consistency across products`}),`
+`,(0,c.jsx)(t.li,{children:`predictable behavior for users`}),`
+`,(0,c.jsx)(t.li,{children:`accessibility and Fluent v9 alignment`}),`
+`,(0,c.jsx)(t.li,{children:`faster implementation with shared conventions`}),`
+`]}),`
+`,(0,c.jsx)(t.h2,{id:`getting-started`,children:`Getting Started`}),`
+`,(0,c.jsxs)(t.ol,{children:[`
+`,(0,c.jsx)(t.li,{children:`Browse stories in the sidebar by package or pattern.`}),`
+`,(0,c.jsx)(t.li,{children:`Use controls to explore variants and props.`}),`
+`,(0,c.jsx)(t.li,{children:`Apply these patterns as-is unless your team has an approved exception.`}),`
+`]})]})}function s(e={}){let{wrapper:t}={...n(),...e.components};return t?(0,c.jsx)(t,{...e,children:(0,c.jsx)(o,{...e})}):o(e)}var c;function l(){return(l=e((()=>{c=t(),r(),i()})))()}l();export{s as default};
---
+++ storybook/assets/iframe-C12NBWD1.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-EjK_BGaz.js";e();
